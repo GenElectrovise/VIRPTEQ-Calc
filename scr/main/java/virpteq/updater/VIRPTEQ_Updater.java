3c347339504f41719a7d1ecf92737ebc44963f14
GenElectrovise <sanchez.pamelos@outlook.com>
2019-11-22 19:42:12 +0000

    Update to updater

--- scr/main/java/virpteq/updater/VIRPTEQ_Updater.java
+++ scr/main/java/virpteq/updater/VIRPTEQ_Updater.java
@@ -12,8 +12,7 @@ import java.util.Map;
 public class VIRPTEQ_Updater {
 
 	public static void main(String[] args) {
-		VIRPTEQ_Updater_GUI.appendLog("Attempting connection");
-		System.out.println("Starting");
+		VIRPTEQ_Updater_GUI.appendLog("Initiating update...");
 		initUpdate();
 	}
 
@@ -35,25 +34,34 @@ public class VIRPTEQ_Updater {
 
 	public static void fetchFromGithub(String fileName, String version) throws Throwable {
 		fileName = "VIRPTEQ_Calculator_" + version + ".jar";
+		VIRPTEQ_Updater_GUI.appendLog("Will search for file: " + fileName);
 		String link = "https://github.com/GenElectrovise/VIRPTEQ-Calc/releases/download/" + version + "/" + fileName; //https://github.com/GenElectrovise/VIRPTEQ-Calc/releases/download/1.2/VIRPTEQ_Calculator_1.2.jar
+		VIRPTEQ_Updater_GUI.appendLog("Will search for file: " + fileName + " :On site: " + link);
 		URL url = new URL(link);
 		HttpURLConnection http = (HttpURLConnection) url.openConnection();
+		VIRPTEQ_Updater_GUI.appendLog("Connection established");
 		Map<String, List<String>> header = http.getHeaderFields();
 		while (isRedirected(header)) {
+			VIRPTEQ_Updater_GUI.appendLog("Being redirected by GitHub");
 			link = header.get("Location").get(0);
 			url = new URL(link);
 			http = (HttpURLConnection) url.openConnection();
 			header = http.getHeaderFields();
 		}
+		VIRPTEQ_Updater_GUI.appendLog("Opening InputStream!");
 		InputStream input = http.getInputStream();
 		byte[] buffer = new byte[4096];
+		VIRPTEQ_Updater_GUI.appendLog("Byte buffer set");
 		int n = -1;
 		File fileOut = new File("scr/main/resources/from_git/" + fileName);
-		VIRPTEQ_Updater_GUI.appendLog(fileOut.getAbsolutePath());
+		VIRPTEQ_Updater_GUI.appendLog("Got file");
+		VIRPTEQ_Updater_GUI.appendLog("Writing file to: " + fileOut.getAbsolutePath());
 		OutputStream output = new FileOutputStream(fileOut);
+		VIRPTEQ_Updater_GUI.appendLog("Opening OutputStream. Writing!");
 		while ((n = input.read(buffer)) != -1) {
 			output.write(buffer, 0, n);
 		}
+		VIRPTEQ_Updater_GUI.appendLog("Written. Closing writer."); //https://github.com/GenElectrovise/VIRPTEQ-Calc
 		output.close();
 	}
 }
